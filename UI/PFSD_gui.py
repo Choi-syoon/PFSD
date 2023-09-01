85542fd95f78d795adff8b992de9053fe2447cce
Choi-syoon <zeprex06@gmail.com>
2023-09-01 14:17:56 +0900

    Combobox select event 적용 안됨

--- UI/PFSD_gui.py
+++ UI/PFSD_gui.py
@@ -1,5 +1,11 @@
+import serial
+import serial.tools.list_ports as sp
+import threading
+import tkinter as tk
 import customtkinter as ctk
 
+
+#-----Main-----
 main = ctk.CTk() 
 main.title("PFSD Desktop GUI")
 main.geometry("960x640")
@@ -7,7 +13,7 @@ main.resizable(False, False)
 main._set_appearance_mode('dark')
 
 
-# Functions
+# -----Functions-----
 
 
 # Options
@@ -15,9 +21,24 @@ main._set_appearance_mode('dark')
 # btn_canny.grid(row=1, column=0)
 
 
-# Sub-section
+# -----Sub-section-----
 sub_panel = ctk.CTkFrame(main)
 sub_panel.pack(side='bottom', padx=100)
 
+# -----Serial Communicate section-----
+def Find_Serial_ports():
+    serial_list = sp.comports()
+    available_port = []
+    for p in serial_list:
+        available_port.append(p.device)
+    return available_port
+
+def on_combobox_select(event):
+    selected_value = Serial_Cbx.get()
+    print("Selected value:", selected_value)
+
+Serial_Cbx = ctk.CTkComboBox(main, values=Find_Serial_ports())
+Serial_Cbx.pack()
+Serial_Cbx.bind("<<ComboboxSelected>>", on_combobox_select)
 
 main.mainloop()
